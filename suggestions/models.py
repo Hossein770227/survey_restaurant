5e161adc6db39a9233f917a1ffe0974610f4d49c
hossein <hossein.h.a770227@gmail.com>
2024-11-29 00:06:39 +0330

    add comments suggestions

--- suggestions/models.py
+++ suggestions/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext as _ 
 
-# Create your models here.
+
+
+class CommentsSuggestions(models.Model):
+    text = models.TextField(_("text"))
+    date_time_created = models.DateTimeField(_("date time created"), auto_now_add=True)
+
+   
+    class Meta:
+        verbose_name =_("comments and suggestions ")
+        verbose_name_plural = _("comments and suggestions ")
+
+    def __str__(self):
+        return self.date_time_created
+    
