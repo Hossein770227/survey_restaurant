5d6d707d9de383a27ecc05d6c1f68d2b27929913
hossein <hossein.h.a770227@gmail.com>
2024-11-28 17:59:47 +0330

    add static files

--- config/settings.py
+++ config/settings.py
@@ -109,19 +109,23 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa'
+# LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
-
+TIME_ZONE = 'Asia/Tehran'
+# TIME_ZONE = 'UTC'
 USE_I18N = True
+USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- survey/admin.py
+++ survey/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import  Food, FoodQuality
+
+
+
+@admin.register(Food)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title','is_active',]
+    search_fields = ['title']
+
+
+
+@admin.register(FoodQuality)
+class FoodQualityAdmin(admin.ModelAdmin):
+    list_display = ['food','choice_quality','is_active',]
+    search_fields = ['title']
